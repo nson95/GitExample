5975b90fe952836b50cd626facbf14ec35fcd44e
nson95 <nicksonnenberg95@gmail.com>
2018-10-04 10:00:16 -0400

    fixed some verbage

--- src/main/java/dice/PigDiceAppApplication.java
+++ src/main/java/dice/PigDiceAppApplication.java
@@ -2,7 +2,6 @@ package dice;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
@@ -17,7 +16,7 @@ public class PigDiceAppApplication {
 		System.out.println();
 		Scanner sc = new Scanner(System.in);
 		String choice="y";
-		
+
 		ArrayList<Integer> highScores=new ArrayList<>();
 		while (choice.equalsIgnoreCase("y")) {
 			System.out.print("How many games would you like to play? ");
@@ -25,21 +24,21 @@ public class PigDiceAppApplication {
 			int rollCount = 0;
 			int score = 0;
 			ArrayList<Integer> scores=new ArrayList<>();
-			while (games>0) {
-				int rollValue = rollOfTheDie();
-				if (rollValue!=1) {
-					rollCount+=1;
-					score+=rollValue;
-					scores.add(score);
-				} else if (rollValue==1) {
-					games-=1;
+				while (games>0) {
+					int rollValue = rollOfTheDie();
+						if (rollValue!=1) {
+							rollCount+=1;
+							score+=rollValue;
+							scores.add(score);
+						} else if (rollValue==1) {
+							games-=1;
+						}
 				}
-			}
 			int highScore = Collections.max(scores);
 			highScores.add(highScore);
 			int highestScore = Collections.max(highScores);
 			System.out.println("Your high score: " +highScore +"\n"
-								+"Highest score: " +highestScore);
+								+"Your overall highest score: " +highestScore);
 			System.out.println();
 			System.out.print("Continue? (y/n) ");
 			choice=sc.next();
@@ -47,8 +46,9 @@ public class PigDiceAppApplication {
 		System.out.println("Goodbye!");
 		sc.close();
 	}
+
 	public static int rollOfTheDie() {
 		return (int) (Math.random() * 6 + 1);
 	}
-	
+
 }
